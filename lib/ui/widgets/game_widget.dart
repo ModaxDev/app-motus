7ba5dc941b1f328c744889389d8019cc3d147156
Modax59 <61698516+ModaxDev@users.noreply.github.com>
2022-05-27 19:04:41 +0200

    feat: refacto

--- lib/ui/widgets/game_widget.dart
+++ lib/ui/widgets/game_widget.dart
@@ -11,15 +11,6 @@ class GameWidget extends StatefulWidget {
     required this.onNewGame,
   }) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final List<String> listWord;
 
   final Function onNewGame;
@@ -46,8 +37,6 @@ class _Game extends State<GameWidget> {
             widget.onNewGame();
             isEndGame = false;
           }), child: Text('Continue to play'))
-
-
         ],
       );
     }
@@ -57,7 +46,6 @@ class _Game extends State<GameWidget> {
         setState(() {
           tries = value + 1;
           isEndGame = true;
-          //divide 100 by the number of tries
           score = score+100 ~/ tries;
         });
       },
